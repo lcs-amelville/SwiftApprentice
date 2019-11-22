784cd0cbf5bca1db7f5119de6826777f726c2ace
aidan melville <amelville@lcs.on.ca>
2019-11-22 15:17:48 -0500

    Done challenges 1 and 2

--- SwiftApprentice/ChapterOne.playground/Contents.swift
+++ SwiftApprentice/ChapterOne.playground/Contents.swift
@@ -13,10 +13,25 @@ let evenOdd = testNumber
 //4
 var anwser = 0
 anwser += 1
-anwser + 10
-anwser * 10
+anwser += 10
+anwser *= 10
 anwser
 print (anwser)
 
+// 1 Declare a constant Int called myAge and set it equal to your age. Also declare an Int variable called dogs and set it equal to the number of dogs you own. Then imagine you bought a new puppy and increment the dogs variable by one.
 
+let myAge = 16
+var dogs = 1
+dogs += 1
+print (dogs)
+
+
+// 2 Given the following code:
+//Modify the first line so that it compiles. Did you use var or let?
+var age = 16
+print (age)
+age = 30
+print (age)
+
+//3 
 
