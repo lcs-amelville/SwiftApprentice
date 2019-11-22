5e5ed6032ae4f35c884c6a76444061e856910d9a
aidan melville <amelville@lcs.on.ca>
2019-11-22 15:13:53 -0500

    started the Mini-exersizes of chapter one

---
+++ SwiftApprentice/ChapterOne.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+//Chapter One Playground and Challenges
+//Mini-exercises
+//1
+let myAge = 16
+//2
+var averageAge = 16
+averageAge = 30
+//3
+let testNumber = 10
+let evenOdd = testNumber
+//4
+var anwser = 0
+anwser += 1
+anwser + 10
+anwser * 10
+anwser
+print (anwser)
+
+
+
