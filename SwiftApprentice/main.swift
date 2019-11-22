9c198b53d5cc1f27f70d8b83294216da18f93279
aidan melville <amelville@lcs.on.ca>
2019-11-22 14:33:50 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Melville, Aidan on 2019-11-22.
+//  Copyright © 2019 Melville, Aidan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
